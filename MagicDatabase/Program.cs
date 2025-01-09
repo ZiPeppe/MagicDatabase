0617762036ef8ede2ddea2d6c6f27ace53d27567
ZiPeppe <93192043+ZiPeppe@users.noreply.github.com>
2025-01-09 10:19:46 +0100

    Jwt Update

--- MagicDatabase/Program.cs
+++ MagicDatabase/Program.cs
@@ -93,6 +93,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"])),
 
                     };
+
                     // Enable logging for debugging
                     options.Events = new JwtBearerEvents
                     {
